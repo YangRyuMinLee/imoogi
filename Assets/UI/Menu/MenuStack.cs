7aab3af7b22d8b119c8d44599cf1b6a7bc8bb39f
asdfer-1234 <sh__lv@naver.com>
2024-01-12 22:10:22 +0900

    add escape pop

--- Assets/UI/Menu/MenuStack.cs
+++ Assets/UI/Menu/MenuStack.cs
@@ -6,6 +6,8 @@ public class MenuStack : MonoBehaviour
     public Stack<GameObject> menuStack;
     public GameObject initialStack;
 
+    public int Count => menuStack.Count;
+
     void Awake()
     {
         menuStack = new Stack<GameObject>();
@@ -19,27 +21,46 @@ public class MenuStack : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pop();
+        }
+    }
+
+    public GameObject Peek() => menuStack.Peek();
+
     public void Push(GameObject menu)
     {
         if(menuStack.TryPeek(out GameObject top)){
             top.SetActive(false);
         }
         menuStack.Push(menu);
-        menuStack.Peek().SetActive(true);
+        Peek().SetActive(true);
     }
 
     public void Pop()
     {
-        if(menuStack.Count == 0){
+        if(Count == 0){
             Debug.LogError("Nothing to pop in MenuStack!");
             return;
         }
-        menuStack.Peek().SetActive(false);
+        Peek().SetActive(false);
         menuStack.Pop();
-        if(menuStack.Count != 0){
-            menuStack.Peek().SetActive(true);
+        if(Count != 0){
+            Peek().SetActive(true);
         }
     }
 
-
+    public void PopUntil(GameObject element)
+    {
+        while (Count != 0 && element != Peek())
+        {
+            Pop();
+        }
+        if(Count != 0){
+            Pop();
+        }
+    }
 }
