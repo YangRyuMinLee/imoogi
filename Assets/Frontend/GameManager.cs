0c22aa15cc3921b60631d4aa47abaec9ad39283c
EGGnmad <viewnono1219@gmail.com>
2024-01-15 09:37:59 +0900

    asdf

---
+++ Assets/Frontend/GameManager.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Game game;
+
+    private void Start()
+    {
+        // for test
+        game = new Game();
+    }
+
+    private void Update()
+    {
+    }
+}
