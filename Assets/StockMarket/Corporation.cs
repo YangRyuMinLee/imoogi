05b5579de866413ae24e98f46df0bd1892938637
EGGnmad <viewnono1219@gmail.com>
2024-01-15 21:27:04 +0900

    change increase rate

--- Assets/StockMarket/Corporation.cs
+++ Assets/StockMarket/Corporation.cs
@@ -8,7 +8,7 @@ public class Corporation
     private string name; // 회사 이름
     private CorporationType type; // 회사 종류
     private int parValue; // 액면가
-    public float maxIncreaseRate = 0.03f; // 틱 당 최대 증가/감소률
+    public float maxIncreaseRate = 0.031f; // 틱 당 최대 증가/감소률
     public float minIncreaseRate = -0.03f; // 틱 당 최소 증가/감소률
     public List<int> priceHistory;
     
