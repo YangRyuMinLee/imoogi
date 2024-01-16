413eea378586b8a099a7463ed459eb8acc9baf4e
asdfer-1234 <sh__lv@naver.com>
2024-01-16 11:02:56 +0900

    corporation effects

--- Assets/StockMarket/Corporation.cs
+++ Assets/StockMarket/Corporation.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [Serializable]
 public class Corporation
@@ -8,28 +10,55 @@ public class Corporation
     private CorporationType type; // 회사 종류
     private int parValue; // 액면가
     public float maxIncreaseRate = 0.01f; // 틱 당 최대 증가/감소률
+    public float EffectedMaxIncreaseRate{
+        get {
+            float total = maxIncreaseRate;
+            effects.ForEach(x => total += x.maxIncreaseRateIncrease);
+            return total;
+        }
+    }
     public float minIncreaseRate = -0.01f; // 틱 당 최소 증가/감소률
+    public float EffectedMinIncreaseRate{
+        get {
+            float total = minIncreaseRate;
+            effects.ForEach(x => total += x.minIncreaseRateIncrease);
+            return total;
+        }
+    }
     
     public string Name => name;
     public CorporationType Type => type;
     public int ParValue => parValue;
+
+    private List<CorporationEffect> effects;
 
     public Corporation(string name, CorporationType type, int initialPrice)
     {
         this.name = name;
         this.type = type;
         this.parValue = initialPrice;
+        effects = new();
     }
 
     public void Tick()
     {
-        float value = parValue * UnityEngine.Random.Range(1 + minIncreaseRate, 1 + maxIncreaseRate);
-        parValue = Mathf.RoundToInt(value); // Temporary Implementation
+        IncreaseParValueByRate(UnityEngine.Random.Range(EffectedMinIncreaseRate, EffectedMaxIncreaseRate));
+        for (int i = 0; i < effects.Count; i++) {
+            var effect = effects[i];
+            effect.timer++;
+            effects[i] = effect;
+        }
+        effects.RemoveAll(x => x.timer >= x.duration);
     }
-    
+
+
     /// <param name="rate"> 퍼센트 / 100 </param>
     public void IncreaseParValueByRate(float rate)
     {
         parValue = Mathf.RoundToInt(parValue * (1 + rate));
+    }
+
+    public void ApplyEffect(CorporationEffect effect) {
+        effects.Add(effect);
     }
 }
