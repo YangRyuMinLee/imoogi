8584bdeaec148e70f530fbdced369e7ce256c18a
EGGnmad <viewnono1219@gmail.com>
2024-01-17 19:06:36 +0900

    add all Endings

--- Assets/Frontend/SceneChange/SceneChanger.cs
+++ Assets/Frontend/SceneChange/SceneChanger.cs
@@ -1,8 +1,19 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] private bool clickToMenuScene;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && clickToMenuScene)
+        {
+            ChangeScene("MenuScene");
+        }
+    }
+
     public void ChangeScene(string newScene)
     {
         SceneManager.LoadScene(newScene);
