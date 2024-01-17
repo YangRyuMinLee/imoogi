f08a73311f2b8acea7e04a640f666d54d0a17124
asdfer-1234 <sh__lv@naver.com>
2024-01-17 14:37:36 +0900

    remove redundant async

--- Assets/Frontend/Bar/BarManager.cs
+++ Assets/Frontend/Bar/BarManager.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using Random = System.Random;
-using Task = System.Threading.Tasks.Task;
 
 public class BarManager : MonoBehaviour
 {
@@ -19,7 +15,7 @@ public class BarManager : MonoBehaviour
         gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
-    public async void Show(float duration = 1f)
+    public void Show(float duration = 1f)
     {
         //random get
         int randomNumber = new Random().Next(0, listenList.Count);
