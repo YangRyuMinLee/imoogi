67eb759d403e8b180308b44d121171170b290acf
EGGnmad <viewnono1219@gmail.com>
2024-01-17 09:03:27 +0900

    add BarManager

--- Assets/Frontend/Bar/BarManager.cs
+++ Assets/Frontend/Bar/BarManager.cs
@@ -1,7 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using Random = System.Random;
+using Task = System.Threading.Tasks.Task;
 
 public class BarManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI listenText;
+    [SerializeField] private List<string> listenList;
+
+    public async void Show(float duration = 0.1f)
+    {
+        //random get
+        int randomNumber = new Random().Next(0, listenList.Count);
+        listenText.text = listenList[randomNumber];
+        
+        await Task.Delay(TimeSpan.FromSeconds(duration));
+
+        listenText.text = string.Empty;
+    }
 }
